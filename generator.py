3fe19987c8905dd68d1b95781c2a65625d4d00ea
Shailendra34 <87485422+Shailendra34@users.noreply.github.com>
2021-12-26 20:23:20 +0530

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,14 +1,14 @@
-import Config
-import logging
-from pyromod import listen
+#import Config
+#import logging
+#from pyromod import listen
 from pyrogram import Client, idle
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 
 
-logging.basicConfig(
-    level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
+#logging.basicConfig(
+  #  level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+#)
+#logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 app = Client(
