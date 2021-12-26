fbe9ba09f84a25e556a0d344fdc515e99637d0a4
Shailendra34 <87485422+Shailendra34@users.noreply.github.com>
2021-12-26 20:58:37 +0530

    Add files via upload

--- generator.py
+++ generator.py
@@ -1,14 +1,14 @@
 import Config
-#import logging
-#from pyromod import listen
+import logging
+from pyromod import listen
 from pyrogram import Client, idle
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 
 
-#logging.basicConfig(
-  #  level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-#)
-#logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.basicConfig(
+    level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 app = Client(
@@ -25,11 +25,11 @@ if __name__ == "__main__":
     try:
         app.start()
     except (ApiIdInvalid, ApiIdPublishedFlood):
-        raise Exception("Your API_ID/API_HASH is not valid.")
+        raise Exception("ʏᴏᴜʀ API_ID/API_HASH ɪs ɴᴏᴛ ᴠᴀʟɪᴅ.")
     except AccessTokenInvalid:
-        raise Exception("Your BOT_TOKEN is not valid.")
+        raise Exception("ʏᴏᴜʀ BOT_TOKEN ɪs ɴᴏᴛ ᴠᴀʟɪᴅ.")
     uname = app.get_me().username
-    print(f"@{uname} Started Successfully!")
+    print(f"@{uname} sᴛᴀʀᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ!")
     idle()
     app.stop()
-    print("Bot stopped. Alvida!")
+    print("ʙᴏᴛ sᴛᴏᴘᴘᴇᴅ ᴀʟᴠɪᴅᴀ...")
