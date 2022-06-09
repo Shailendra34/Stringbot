1517bffdc8f09c0bfa78335569ee2069e0dd06dc
Shailendra34 <87485422+Shailendra34@users.noreply.github.com>
2022-06-09 17:07:56 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,14 +4,14 @@ ENVIRONMENT = os.environ.get('ENVIRONMENT', False)
 
 if ENVIRONMENT:
     try:
-        API_ID = int(os.environ.get('API_ID', 0))
+        API_ID = int(os.environ.get("API_ID", "15663735"))
     except ValueError:
         raise Exception("Your API_ID is not a valid integer.")
-    API_HASH = os.environ.get('API_HASH', None)
-    BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
-    DATABASE_URL = os.environ.get('DATABASE_URL', None)
+    API_HASH = os.environ.get("API_HASH", " 41ffd2a4ee5614e9a9269bdb818dcda3")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5015892079:AAFa2B4p7lGv7isApfpzpb0hnsRz9LAE7CE")
+    DATABASE_URL = os.environ.get("DATABASE_URL", None)
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql") 
-    MUST_JOIN = os.environ.get('MUST_JOIN', None)
+    MUST_JOIN = os.environ.get("MUST_JOIN", " HeroOfficialBots")
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN.replace("@", "")
 else:
