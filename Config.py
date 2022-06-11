818ca1e0e228fda336f949280f55666f68f7c27d
Shailendra34 <87485422+Shailendra34@users.noreply.github.com>
2022-06-11 14:45:11 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -9,7 +9,7 @@ if ENVIRONMENT:
         raise Exception("Your API_ID is not a valid integer.")
     API_HASH = os.environ.get("API_HASH", " 41ffd2a4ee5614e9a9269bdb818dcda3")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5015892079:AAFa2B4p7lGv7isApfpzpb0hnsRz9LAE7CE")
-    DATABASE_URL = os.environ.get("DATABASE_URL", None)
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://BOT34:BOT34@cluster0.2dh9k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql") 
     MUST_JOIN = os.environ.get("MUST_JOIN", " HeroOfficialBots")
     if MUST_JOIN.startswith("@"):
@@ -18,7 +18,7 @@ else:
     API_ID = 15663735
     API_HASH = "41ffd2a4ee5614e9a9269bdb818dcda3"
     BOT_TOKEN = "5015892079:AAFa2B4p7lGv7isApfpzpb0hnsRz9LAE7CE"
-    DATABASE_URL = ""
+    DATABASE_URL = "mongodb+srv://BOT34:BOT34@cluster0.2dh9k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
     MUST_JOIN = "HeroOfficialBots"
     if MUST_JOIN.startswith("@"):
